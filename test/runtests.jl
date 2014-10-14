613231961836d01a8a4736e668753c095ed80b2f
Ben Arthur <arthurb@hhmi.org>
2014-10-14 09:24:08 -0400

    made unit test more robuts to different device names

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,50 +1,52 @@
 using NIDAQ, Base.Test
 
-t = analog_input("Dev1/ai0")
+d = devices()[1]
+
+t = analog_input(d*"/ai0")
 @test typeof(t) == NIDAQ.AITask
 @test start(t) == nothing
 @test length(read(t, Float64, 3)) == 3
 @test stop(t) == nothing
-@test analog_input(t, "Dev1/ai1") == nothing
+@test analog_input(t, d*"/ai1") == nothing
 @test start(t) == nothing
 @test length(read(t, Uint32, 6)) == 12
 @test stop(t) == nothing
 @test clear(t) == nothing
 
-t = analog_output("Dev1/ao0")
+t = analog_output(d*"/ao0")
 @test typeof(t) == NIDAQ.AOTask
 @test start(t) == nothing
 @test write(t, rand(3)) == 3
 @test stop(t) == nothing
-@test analog_output(t, "Dev1/ao1") == nothing
+@test analog_output(t, d*"/ao1") == nothing
 @test start(t) == nothing
 @test write(t, rand(Uint32,6,2)) == 6
 @test stop(t) == nothing
 @test clear(t) == nothing
 
-t = digital_input("Dev1/Port0/Line0")
+t = digital_input(d*"/Port0/Line0")
 @test typeof(t) == NIDAQ.DITask
 @test start(t) == nothing
 @test length(read(t, Uint32, 3)) == 3
 @test stop(t) == nothing
-@test digital_input(t, "Dev1/Port0/Line1") == nothing
+@test digital_input(t, d*"/Port0/Line1") == nothing
 @test start(t) == nothing
 @test length(read(t, Uint8, 6)) == 12
 @test stop(t) == nothing
 @test clear(t) == nothing
 
-t = digital_output("Dev1/Port0/Line0")
+t = digital_output(d*"/Port0/Line0")
 @test typeof(t) == NIDAQ.DOTask
 @test start(t) == nothing
 @test write(t, uint32([1,0,1,0,1,0])) == 6
 @test stop(t) == nothing
-@test digital_output(t, "Dev1/Port0/Line1") == nothing
+@test digital_output(t, d*"/Port0/Line1") == nothing
 @test start(t) == nothing
 @test write(t, uint8([1 0; 0 0; 1 0; 0 1; 1 1; 0 1])) == 6
 @test stop(t) == nothing
 @test clear(t) == nothing
 
-t = generate_pulses("Dev1/ctr0")
+t = generate_pulses(d*"/ctr0")
 @test typeof(t) == NIDAQ.COTask
 @test NIDAQ.DAQmxCfgImplicitTiming(t.th, int32(NIDAQ.DAQmx_Val_FiniteSamps), uint64(10)) == 0
 @test start(t) == nothing
