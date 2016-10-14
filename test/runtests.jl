46ba82a5f95803c72d645c64ae83553ffba13eb6
Ben Arthur <arthurb@hhmi.org>
2016-10-14 14:09:38 -0400

    refactor tests to use @testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,22 @@
 using NIDAQ, Base.Test
 
-info("NIDAQ: Checking installation - no measurement hardware is needed for this section")
+@testset "installation" begin
 @test typeof(getproperties()) == Dict{String,Tuple{Any,Bool}}
 @test haskey(getproperties(), "NIDAQMajorVersion")
-d = devices()
+global dev = devices()
+end
 
-if length(d) == 0
-    info("NIDAQ: no measurement HW found, skipping rest of the test suite")
-    device_available = false
+@testset "device" begin
+if length(dev) == 0
+    info("no data acquisition devices found")
+    exit()
 else
-    d=d[1]
-    info("NIDAQ: found at least one measurement HW: $(d)")
-    @test typeof(getproperties(d)) == Dict{String,Tuple{Any,Bool}}
-    pr = getproperties(d)
-    device_available = true
+    info("found $(dev).  testing with $(dev[1])")
+    dev=dev[1]
+    @test typeof(getproperties(dev)) == Dict{String,Tuple{Any,Bool}}
+    global props = getproperties(dev)
 
-    @test typeof(getproperties(d)) == Dict{String,Tuple{Any,Bool}}
+    @test typeof(getproperties(dev)) == Dict{String,Tuple{Any,Bool}}
     @test typeof(analog_input_channels()) == Vector{String}
     @test typeof(analog_output_channels()) == Vector{String}
     @test typeof(digital_input_channels()) == Vector{String}
@@ -25,76 +26,89 @@ else
     @test typeof(analog_input_ranges()) == Matrix{Float64}
     @test typeof(analog_output_ranges()) == Matrix{Float64}
 end
+end
 
 
-if !(device_available && length(pr["AIPhysicalChans"][1]) > 0)
-    info("NIDAQ: measurement HW does not support any AIPhysicalChans, skipping part of the suite")
+@testset "analog input" begin
+if length(props["AIPhysicalChans"][1]) == 0
+    info("$dev does not support AIPhysicalChans")
 else
-    t = analog_input(d*"/ai0")
+    t = analog_input(dev*"/ai0")
     @test typeof(getproperties(t)) == Dict{String,Tuple{Any,Bool}}
-    @test typeof(getproperties(t,d*"/ai0")) == Dict{String,Tuple{Any,Bool}}
+    @test typeof(getproperties(t,dev*"/ai0")) == Dict{String,Tuple{Any,Bool}}
     @test typeof(t) == NIDAQ.AITask
     @test start(t) == nothing
     @test length(read(t, Float64, 3)) == 3
     @test stop(t) == nothing
-    @test analog_input(t, d*"/ai1") == nothing
+    @test analog_input(t, dev*"/ai1") == nothing
     @test start(t) == nothing
     @test length(read(t, UInt32, 6)) == 12
     @test stop(t) == nothing
     @test clear(t) == nothing
 end
+end
 
-if !(device_available && length(pr["AOPhysicalChans"][1]) > 0)
-    info("NIDAQ: measurement HW does not support any AOPhysicalChans, skipping part of the suite")
+@testset "analog output" begin
+if length(props["AOPhysicalChans"][1]) == 0
+    info("$dev does not support AOPhysicalChans")
 else
-    t = analog_output(d*"/ao0")
+    t = analog_output(dev*"/ao0")
     @test typeof(t) == NIDAQ.AOTask
     @test start(t) == nothing
     @test write(t, rand(3)) == 3
     @test stop(t) == nothing
-    @test analog_output(t, d*"/ao1") == nothing
+    @test analog_output(t, dev*"/ao1") == nothing
     @test start(t) == nothing
     @test write(t, rand(UInt32,6,2)) == 6
     @test stop(t) == nothing
     @test clear(t) == nothing
 end
+end
 
-if !(device_available && length(pr["DILines"][1]) > 0)
-    info("NIDAQ: measurement HW does not support DILines, skipping part of the suite")
+
+@testset "digital input" begin
+if length(props["DILines"][1]) == 0
+    info("$dev does not support DILines")
 else
-    t = digital_input(d*"/Port0/Line0")
+    t = digital_input(dev*"/Port0/Line0")
     @test typeof(t) == NIDAQ.DITask
     @test start(t) == nothing
     @test length(read(t, UInt32, 3)) == 3
     @test stop(t) == nothing
-    @test digital_input(t, d*"/Port0/Line1") == nothing
+    @test digital_input(t, dev*"/Port0/Line1") == nothing
     @test start(t) == nothing
     @test length(read(t, UInt8, 6)) == 12
     @test stop(t) == nothing
     @test clear(t) == nothing
 end
-if !(device_available && length(pr["DOLines"][1]) > 0)
-    info("NIDAQ: measurement HW does not support DILines, skipping part of the suite")
+end
+
+@testset "digital output" begin
+if length(props["DOLines"][1]) == 0
+    info("$dev does not support DOLines")
 else
-    t = digital_output(d*"/Port0/Line0")
+    t = digital_output(dev*"/Port0/Line0")
     @test typeof(t) == NIDAQ.DOTask
     @test start(t) == nothing
     @test write(t, round(UInt32, [1,0,1,0,1,0])) == 6
     @test stop(t) == nothing
-    @test digital_output(t, d*"/Port0/Line1") == nothing
+    @test digital_output(t, dev*"/Port0/Line1") == nothing
     @test start(t) == nothing
     @test write(t, round(UInt8, [1 0; 0 0; 1 0; 0 1; 1 1; 0 1])) == 6
     @test stop(t) == nothing
     @test clear(t) == nothing
 end
+end
 
-if !(device_available && length(pr["COPhysicalChans"][1]) > 0)
-    info("NIDAQ: measurement HW does not support COPhysicalChans, skipping part of the suite")
+@testset "counter output" begin
+if length(props["COPhysicalChans"][1]) == 0
+    info("$dev does not support COPhysicalChans")
 else
-    t = generate_pulses(d*"/ctr0")
+    t = generate_pulses(dev*"/ctr0")
     @test typeof(t) == NIDAQ.COTask
     @test NIDAQ.CfgImplicitTiming(t.th, NIDAQ.Val_FiniteSamps, UInt64(10)) == 0
     @test start(t) == nothing
     @test stop(t) == nothing
     @test clear(t) == nothing
+end
 end
